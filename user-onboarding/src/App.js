6672db27e6652d067b3f1b427de9f832a02375b1
Clare Jackson <clare-jackson@lambdastudents.com>
2020-11-04 14:49:52 -0500

    All done except Terms not rendering.

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from './Form'; 
-import './App.css';
+import * as yup from 'yup'
+import schema from './formSchema'
+import axios from 'axios'
 
 
 const initialFormValues = {
@@ -30,14 +32,39 @@ function App() {
   const [users, setUsers] = useState(initialUsers)
   const [formValues, setFormValues] = useState(initialFormValues)
   const [formErrors, setFormErrors] = useState(initialFormErrors)
-  const [disables, setDisabled] = useState(initialDisabled)
+  const [disabled, setDisabled] = useState(initialDisabled)
 
   const postNewUsers = newUser => {
-    //axios.post to ('https://reqres.in/api/users')
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(response => {
+        setUsers([...users, response.data])
+      })
+      .catch(error => {
+        debugger 
+        console.log(error)
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      })
   }
 
   const validate = (name, value) => {
     // yup validation schema
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(valid => {
+        setFormErrors({
+          ...formErrors,
+          [name]: ""
+        })
+      })
+      .catch(error => {
+        setFormErrors({
+          ...formErrors,
+          [name]: error.errors[0]
+        })
+      })
   }
 
   const inputChange = (name, value) => {
@@ -50,15 +77,25 @@ function App() {
 
   const formSubmit = () => {
     const newUser = {
-      name: ,
-      email: ,
-      password: ,
-      terms: 
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      // terms: {checked ? formValues[terms]}
     }
     postNewUsers(newUser)
   }
 
   // Side Effects????
+  useEffect(() => {
+    console.log(formValues)
+  }, [formValues])
+
+  useEffect(() => {
+    schema.isValid(formValues)
+      .then(valid => {
+        setDisabled(!valid)
+      })
+  }, [formValues])
 
   return (
     <div className="App">
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,7 +1,79 @@
 import React, { useState } from 'react';
 
 const Form = (props) => {
+  const {
+    values,
+    submit,
+    change,
+    disabled,
+    errors,
+  } = props
 
+  const onSubmit = event => {
+    event.preventDefault()
+    submit()
+  }
+
+  const onChange = event => {
+    const { name, value, type, checked } = event.target
+    const valueToUse = type === 'checkbox' ? checked : value
+    change(name, valueToUse)
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <h2>User Onbording</h2>
+
+        <button disabled={disabled}>Submit</button>
+
+        <div> 
+          <div>{errors.name}</div>
+          <div>{errors.email}</div>
+          <div>{errors.password}</div>
+          <div>{errors.terms}</div>
+        </div>
+      </div>
+
+      <div>
+        <h2>Registration</h2>
+
+        <label>Name:     </label>
+        <input 
+        value={values.name}
+        onChange={onChange}
+        name='name'
+        type='text'
+        />
+
+        <label>Email:     </label>
+        <input 
+        value={values.email}
+        onChange={onChange}
+        name='email'
+        type='email'
+        />
+
+        <label>Password:     </label>
+        <input 
+        value={values.password}
+        onChange={onChange}
+        name='password'
+        type='password'
+        />
+
+        <label>Terms of Service:     </label>
+        <input 
+        value='true'
+        onChange={onChange}
+        name='terms'
+        type='radio'
+        checked={values.terms === 'true'}
+        />
+
+      </div>
+    </form>
+  )
 
 }
 
